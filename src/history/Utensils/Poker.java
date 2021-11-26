5c684ca5d86e4bdce9f30eedc2e95b6153ef76f9
arnmacing <93253694+arnmacing@users.noreply.github.com>
2021-11-26 12:31:57 +0300

    Update Poker.java

--- src/history/Utensils/Poker.java
+++ src/history/Utensils/Poker.java
@@ -6,4 +6,20 @@ public class Poker implements Utensils {
     public String getName() {
         return "кочерга";
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return !(obj == null || getClass() != obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return "Действие полетели";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getName().hashCode() * 101;
+    }
 }
