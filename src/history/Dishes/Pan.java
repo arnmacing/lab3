421fc63a41c166e2af31148de675170175c8b813
arnmacing <93253694+arnmacing@users.noreply.github.com>
2021-11-26 12:35:47 +0300

    Update Pan.java

--- src/history/Dishes/Pan.java
+++ src/history/Dishes/Pan.java
@@ -3,4 +3,20 @@ package history.Dishes;
 public class Pan implements Dishes {
     @Override
     public String getName() { return "Сковородки"; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return !(obj == null || getClass() != obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return "Действие посыпались";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getName().hashCode() * 127;
+    }
 }
