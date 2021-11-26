ef459b05d87df8521dfc834e3007e1c827ddc1fd
arnmacing <93253694+arnmacing@users.noreply.github.com>
2021-11-26 12:31:38 +0300

    Update Scoop.java

--- src/history/Utensils/Scoop.java
+++ src/history/Utensils/Scoop.java
@@ -5,4 +5,20 @@ public class Scoop implements Utensils {
     public String getName() {
         return "совок";
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return !(obj == null || getClass() != obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return "Действие полетели";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getName().hashCode() * 101;
+    }
 }
