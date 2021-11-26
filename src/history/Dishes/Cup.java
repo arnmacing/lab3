67f5b87ffff161ce53f78017ddcf625b5c922760
arnmacing <93253694+arnmacing@users.noreply.github.com>
2021-11-26 12:35:29 +0300

    Update Cup.java

--- src/history/Dishes/Cup.java
+++ src/history/Dishes/Cup.java
@@ -2,5 +2,23 @@ package history.Dishes;
 
 public class Cup implements Dishes {
     @Override
-    public String getName() { return "Кружки"; }
+    public String getName() {
+        return "Кружки";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return !(obj == null || getClass() != obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return "Действие посыпались";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getName().hashCode() * 127;
+    }
 }
