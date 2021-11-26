c341dbf3660f9b990da3d57bb62532b847690c6f
arnmacing <93253694+arnmacing@users.noreply.github.com>
2021-11-26 12:36:48 +0300

    Update Plate.java

--- src/history/Dishes/Plate.java
+++ src/history/Dishes/Plate.java
@@ -3,4 +3,21 @@ package history.Dishes;
 public class Plate implements Dishes {
     @Override
     public String getName() { return "Тарелки"; };
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return !(obj == null || getClass() != obj.getClass());
+    }
+
+    @Override
+    public String toString() {
+        return "Действие посыпались";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getName().hashCode() * 127;
+    }
 }
+
